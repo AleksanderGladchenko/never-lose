7874d6bc104dfbc846c7f056ffe912ddd7e06790
Kotik228322 <135319167+Kotik228322@users.noreply.github.com>
2023-10-05 20:21:51 +0300

    90% 3/18 pages

---
+++ game_page(popups)/popups.js
@@ -0,0 +1,7 @@
+var button = document.querySelector("#profile");
+
+// Добавляем обработчик события клика на кнопку
+button.addEventListener("click", function() {
+    alert("Кнопка нажата!");
+});
+
